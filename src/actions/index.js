44f60b1fe624ed3bb912a1fc40d4853efc43366c
Jae Huh <jaeeunhuh@gmail.com>
2017-08-14 15:56:17 +1000

    Create helper functions to dispatch an action when login fails or succeeds - 16.121

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,6 +3,8 @@ import firebase from 'firebase'
 import {
   EMAIL_CHANGED,
   PASSWORD_CHANGED,
+  LOGIN_USER_FAIL,
+  LOGIN_USER_SUCCESS,
 } from './types'
 
 export const emailChanged = (text) => {
@@ -22,8 +24,22 @@ export const passwordChanged = (text) => {
 export const loginUser = ({ email, password }) => {
   return (dispatch) => {
     firebase.auth().signInWithEmailAndPassword(email, password)
-      .then((user) => {
-        dispatch({ type: 'LOGIN_USER_SUCCESS', payload: user }) // An action must be manually dispatched once response is returned for asynch calls.
+      .then(user => loginUserSuccess(dispatch, user))
+      .catch(() => {
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+          .then(user => loginUserSuccess(dispatch, user))
+          .catch(() => loginUserFail(dispatch))
       })
   }
+}
+
+const loginUserFail = (dispatch) => {
+  dispatch({ type: LOGIN_USER_FAIL })
+}
+
+const loginUserSuccess = (dispatch, user) => {
+  dispatch({
+    type: LOGIN_USER_SUCCESS,
+    payload: user,
+  })
 }
--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -1,11 +1,13 @@
 import {
   EMAIL_CHANGED,
   PASSWORD_CHANGED,
+  LOGIN_USER_SUCCESS,
 } from '../actions/types'
 
 const initialState = {
   email: '',
   password: '',
+  user: null,
 }
 
 export default (state = initialState, action) => {
@@ -22,6 +24,13 @@ export default (state = initialState, action) => {
         ...state,
         password: action.payload,
       }
+
+    case LOGIN_USER_SUCCESS:
+      return {
+        ...state,
+        user: action.payload,
+      }
+
     default:
       return state
   }
