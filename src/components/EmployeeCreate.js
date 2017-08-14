edb5a23ef303205676b77f2cdd8e2aa4961329cf
Jae Huh <jaeeunhuh@gmail.com>
2017-08-14 18:34:07 +1000

    Create dummy employee create component using reusable components

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,12 +1,34 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+
+import { Card, CardSection, Input, Button } from './common'
 
 class EmployeeCreate extends React.Component {
   render() {
     return (
-      <View>
-        <Text>Employee Form</Text>
-      </View>
+      <Card>
+        <CardSection>
+          <Input
+            label="Name"
+            placeholder="Jae"
+          />
+        </CardSection>
+
+        <CardSection>
+          <Input
+            label="Phone"
+            placeholder="021 000 0000"
+          />
+        </CardSection>
+
+        <CardSection>
+        </CardSection>
+
+        <CardSection>
+          <Button>
+            Create
+          </Button>
+        </CardSection>
+      </Card>
     )
   }
 }
