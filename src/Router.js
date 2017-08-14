e475ad1c74a1ee12ab294c83550b2d42eeb8f8be
Jae Huh <jaeeunhuh@gmail.com>
2017-08-14 17:58:16 +1000

    Add employee list scene in routes file

--- src/Router.js
+++ src/Router.js
@@ -2,12 +2,14 @@ import React from 'react'
 import { Scene, Router } from 'react-native-router-flux'
 
 import LogInForm from './components/LogInForm'
+import EmployeeList from './components/EmployeeList'
 
 const RouterComponent = () => {
   return (
     // sceneStyle in Router component applies to all scenes
     <Router sceneStyle={{ paddingTop: 65 }}>
       <Scene key="login" component={LogInForm} title="Please Login" />
+      <Scene key="employeeList" component={EmployeeList} title="Employees" />
     </Router>
   )
 }
