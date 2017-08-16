91199722a3acba2e87773a9ea1f2a4f38a11291b
Jae Huh <jaeeunhuh@gmail.com>
2017-08-16 11:51:03 +1000

    Prepopulate employee update form using the employee info saved in the store - 19.154

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -1,16 +1,29 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import _ from 'lodash'
 
 import { Card, CardSection, Button } from './common'
 import EmployeeForm from './EmployeeForm'
+import { employeeUpdate } from '../actions'
 
 class EmployeeEdit extends React.Component {
+  componentWillMount() {
+    _.each(this.props.employee, (value, prop) => {
+      this.props.employeeUpdate({ prop, value })
+    })
+  }
+
+  onButtonPress() {
+    const { name, phone, shift } = this.props
+    console.log(name, phone, shift)
+  }
+
   render() {
     return (
       <Card>
         <EmployeeForm />
         <CardSection>
-          <Button>
+          <Button onPress={this.onButtonPress.bind(this)}>
             Save Changes
           </Button>
         </CardSection>
@@ -19,4 +32,9 @@ class EmployeeEdit extends React.Component {
   }
 }
 
-export default connect()(EmployeeEdit)
+const mapStateToProps = (state) => {
+  const { name, phone, shift } = state.employeeForm
+  return { name, phone, shift }
+}
+
+export default connect(mapStateToProps, { employeeUpdate })(EmployeeEdit)
